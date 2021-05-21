649d7512a565c1a833ed78937928b081fe913453
Christian Peterson <25235970+ChrisRPeterson@users.noreply.github.com>
2021-05-21 10:20:42 -0500

    Setup for eslint and prettier

---
+++ server/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+  },
+  plugins: ['react'],
+  rules: {},
+};
