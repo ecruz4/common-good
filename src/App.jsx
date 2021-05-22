2491eb702b4a8bc9a86f02e7fb62f0619ed2cd75
Dylan Reid <dylanreid7@gmail.com>
2021-05-22 14:22:13 -0700

    Added input fields and improved styling.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Map from './details/map';
+import Donations from './details/donations';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
+      <Donations />
       <Map />
     </div>
   );
--- src/details/map.jsx
+++ src/details/map.jsx
@@ -4,7 +4,33 @@ import GoogleMapReact from 'google-map-react';
 import config from '../config';
 /* eslint-disable react/prop-types */
 
-const RandomComponent = ({ text }) => <div>{text}</div>;
+const RandomComponent = () => (
+  <div
+    style={{
+      // padding:10,
+      // margin:20,
+      // display:"inline-block",
+      // backgroundColor: "#1C89BF",
+      // borderRadius: "50%",
+      // width:5,
+      // height:5,
+      // border: "2px solid #FFFFFF"
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      width: '18px',
+      height: '18px',
+      backgroundColor: '#1C89BF',
+      border: '2px solid #fff',
+      borderRadius: '100%',
+      userSelect: 'none',
+      // transform: translate(-50%, -50%),
+      // &:hover {
+      //   z-index: 1,
+      // }
+    }}
+  />
+);
 
 export default function Map() {
   const apiKey = config.googleApiKey;
@@ -29,7 +55,6 @@ export default function Map() {
           <RandomComponent
             lat={latitude}
             lng={longitude}
-            text="My Marker"
           />
         </GoogleMapReact>
       </div>
