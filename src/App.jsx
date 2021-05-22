addf61a5e5d19e3b64238a2b4ed8838397753610
Pongsak Pattamasaevi <ppattamasaevi@gmail.com>
2021-05-23 04:37:28 +0800

    Starting implementing the RequestTile

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import RequestTile from './RequestTile';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
+      <RequestTile/>
     </div>
   );
 }
---
+++ src/RequestTile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import * from './db/firebase';
+
+/*
+- Charity request tile (profile page)**
+    - Data required
+        - Request object (mapped over in parent component)
+            - title
+            - date
+            - description
+            - quantity
+            - emergency
+*/
+
+console.log(firestore);
+
+const RequestTile = (  ) => {
+
+
+
+  // return (
+
+  // );
+
+}
+
+
+
+
+export default RequestTile;
