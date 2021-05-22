36f084d74fb54cc983bab48b71c271767cc9cb92
Christian Peterson <25235970+ChrisRPeterson@users.noreply.github.com>
2021-05-22 12:49:12 -0500

    Resolved conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 
 function App() {
-<<<<<<< HEAD
-
-=======
->>>>>>> 7a63384e44d7220a403af3eb47da3f80d72475d5
   return (
     <div className="App">
       <h1>Hello World!</h1>
