54219565abf082a5da55c16522c8f4625de413e9
Pongsak Pattamasaevi <ppattamasaevi@gmail.com>
2021-05-26 07:37:33 +0800

    Fixed bug in AllOffers whereby time to expiry does now show on tiles when user provides searchTerm.

--- src/components/AllOffers.jsx
+++ src/components/AllOffers.jsx
@@ -5,55 +5,64 @@ import OfferTile from './tiles/OfferTile';
 import capitalize from '../utils/capitalize';
 import { expiryThreshold } from '../utils/moment';
 
-
 const AllOffers = ({ uid, searchTerm }) => {
-
   const [docs, setDocs] = useState([]);
   const retrievedDocs = [];
 
   const findAll = () => {
-    firestore.firestore.collection('offers').orderBy('date', 'asc')
+    firestore.firestore
+      .collection('offers')
+      .orderBy('date', 'asc')
       .get()
       .then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
-          console.log(doc.data())
           const createTimeInMs = doc.data().date.toMillis();
           // filter for listings no older than 60 days
           if (createTimeInMs > expiryThreshold) {
             const docWithExpiryDate = {
               ...doc.data(),
-              expiry: createTimeInMs - expiryThreshold
-            }
+              expiry: createTimeInMs - expiryThreshold,
+            };
             retrievedDocs.push(docWithExpiryDate);
           }
         });
         setDocs(retrievedDocs);
       })
-      .catch((err) => console.log(err.message))
-  }
+      .catch((err) => console.log(err.message));
+  };
 
   const findByCriteria = (field, operator, term) => {
-    firestore.firestore.collection('offers').where(field, operator, term).orderBy('date', 'asc')
+    firestore.firestore
+      .collection('offers')
+      .where(field, operator, term)
+      .orderBy('date', 'asc')
       .get()
       .then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
-          retrievedDocs.push(doc.data());
+          const createTimeInMs = doc.data().date.toMillis();
+          // filter for listings no older than 60 days
+          if (createTimeInMs > expiryThreshold) {
+            const docWithExpiryDate = {
+              ...doc.data(),
+              expiry: createTimeInMs - expiryThreshold,
+            };
+            retrievedDocs.push(docWithExpiryDate);
+          }
         });
         setDocs(retrievedDocs);
       })
-      .catch((err) => console.log(err.message))
-  }
+      .catch((err) => console.log(err.message));
+  };
 
   const findByName = (term) => {
     findByCriteria('title', '==', capitalize(term));
-  }
+  };
 
   const findByUid = (id) => {
     findByCriteria('donor_id', '==', id);
-  }
+  };
 
   useEffect(() => {
-
     // if a donor's uid is passed as prop, component will render all OfferTiles related to that donor.
     // if no uid is provided, component will default to listening to searches by item name.
     // if no search term exists, it will render all OfferTiles, prioritizing older listings (close to expiry).
@@ -69,13 +78,11 @@ const AllOffers = ({ uid, searchTerm }) => {
 
   return (
     <Grid container spacing={3}>
-      {docs.map((doc) =>
-        <OfferTile doc={doc} key={doc.title}/>
-      )}
+      {docs.map((doc) => (
+        <OfferTile doc={doc} key={doc.title} />
+      ))}
     </Grid>
   );
-
-}
-
+};
 
 export default AllOffers;
