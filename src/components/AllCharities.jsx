76af1b5cfd289aed73efa912f3ca7f8600e56c71
Pongsak Pattamasaevi <ppattamasaevi@gmail.com>
2021-05-27 04:44:35 +0800

    Basic pagination implemented for AllCharities

--- src/components/AllCharities.jsx
+++ src/components/AllCharities.jsx
@@ -5,22 +5,54 @@ import CharityTile from './tiles/CharityTile';
 import capitalize from '../utils/capitalize';
 
 const AllCharities = ({ searchTerm, criteria }) => {
-  const [docs, setDocs] = useState([]);
   const allDocs = [];
+  const [docs, setDocs] = useState([]);
+  const [lastVisibleDoc, setLastVisibleDoc] = useState({});
+  const [noMoreDocs, setNoMoreDocs] = useState(false);
 
-  const findAllCharities = () => {
-    firestore.firestore.collection("organizations").get()
+  const findInitialCharities = () => {
+    firestore.firestore
+      .collection('organizations')
+      .orderBy('name', 'asc')
+      .limit(2)
+      .get()
       .then((querySnapshot) => {
+        setLastVisibleDoc(querySnapshot.docs[querySnapshot.docs.length - 1]);
         querySnapshot.forEach((doc) => {
           allDocs.push(doc.data());
         });
         setDocs(allDocs);
       })
-      .catch((err) => console.log(err.message))
-  }
+      .catch((err) => console.log(err.message));
+  };
+
+  const findMoreCharities = () => {
+    firestore.firestore
+      .collection('organizations')
+      .orderBy('name', 'asc')
+      .startAfter(lastVisibleDoc)
+      .limit(2)
+      .get()
+      .then((querySnapshot) => {
+        const numOfDocsFetched = querySnapshot.docs.length;
+        if (numOfDocsFetched > 0) {
+          setLastVisibleDoc(querySnapshot.docs[querySnapshot.docs.length - 1]);
+          querySnapshot.forEach((doc) => {
+            allDocs.push(doc.data());
+          });
+          setDocs([...docs, ...allDocs]);
+        } else {
+          console.log('No more docs to fetch.');
+          setNoMoreDocs(true);
+        }
+      })
+      .catch((err) => console.log(err.message));
+  };
 
   const findCharityByCriteria = (field, operator, term) => {
-    firestore.firestore.collection("organizations").where(field, operator, term)
+    firestore.firestore
+      .collection('organizations')
+      .where(field, operator, term)
       .get()
       .then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
@@ -28,49 +60,55 @@ const AllCharities = ({ searchTerm, criteria }) => {
         });
         setDocs(allDocs);
       })
-      .catch((err) => console.log(err.message))
-  }
+      .catch((err) => console.log(err.message));
+  };
 
   const findCharitiesByName = (term) => {
-    findCharityByCriteria("search_name", "array-contains", term);
-  }
+    findCharityByCriteria('search_name', 'array-contains', term);
+  };
 
   const findCharitiesByCity = (term) => {
-    findCharityByCriteria("city", "==", capitalize(term));
-  }
+    findCharityByCriteria('city', '==', capitalize(term));
+  };
 
   const findCharitiesByState = (term) => {
-    findCharityByCriteria("state", "==", term.toUpperCase())
-  }
+    findCharityByCriteria('state', '==', term.toUpperCase());
+  };
 
   const findCharitiesByTheme = (term) => {
-    findCharityByCriteria("focus", "==", capitalize(term))
-  }
+    findCharityByCriteria('focus', '==', capitalize(term));
+  };
 
   const searchSelection = {
     name: findCharitiesByName,
     city: findCharitiesByCity,
     state: findCharitiesByState,
-    theme: findCharitiesByTheme
+    theme: findCharitiesByTheme,
   };
 
   useEffect(() => {
     if (!searchTerm || searchTerm === '') {
-      findAllCharities();
+      findInitialCharities();
     } else {
       searchSelection[criteria](searchTerm);
     }
   }, [searchTerm]);
 
   return (
-    <Grid container spacing={3}>
-      {docs.map((doc) =>
-        <CharityTile doc={doc} key={doc.name}/>
+    <>
+      <Grid container spacing={3}>
+        {docs.map((doc) => (
+          <CharityTile doc={doc} key={doc.name} />
+        ))}
+      </Grid>
+      <br />
+      {noMoreDocs ? (
+        <div>All Charities Displayed</div>
+      ) : (
+        <div onClick={() => findMoreCharities()}>More</div>
       )}
-    </Grid>
+    </>
   );
-
-}
-
+};
 
 export default AllCharities;
