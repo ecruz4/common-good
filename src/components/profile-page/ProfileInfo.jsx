bef5b243ffa6921f858134bbd6f50f33544ad3ce
eric-cruz <eacruz@live.com>
2021-05-27 20:32:37 -0500

    Finished styling and added a ForumIcon to start chat with other users when viewing their profile

--- src/components/profile-page/ProfileInfo.jsx
+++ src/components/profile-page/ProfileInfo.jsx
@@ -1,13 +1,34 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, {useContext} from 'react';
 import zipcodes from 'zipcodes';
-import { Grid, Typography } from '@material-ui/core';
+import { Grid, Typography, IconButton } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import ForumIcon from '@material-ui/icons/Forum';
+import UserContext from '../../contexts/UserContext';
 
 function ProfileInfo({ data }) {
   const location = zipcodes.lookup(data.zipcode);
 
+  const { userInfo, setUserInfo } = useContext(UserContext);
+
   return (
     <Grid item container justify="flex-end" spacing={8} align="center">
+      {userInfo.uid === data.uid ? null :
+      <Grid item>
+        <Link
+          key="chat"
+          to={{
+            pathname: `/chat/${data.uid}`,
+            state: {
+              userId: data.uid,
+            },
+          }}
+        >
+          <IconButton aria-label="chat">
+            <ForumIcon />
+          </IconButton>
+        </Link>
+      </Grid>}
       <Grid item>
         <Typography
           color="textSecondary"
